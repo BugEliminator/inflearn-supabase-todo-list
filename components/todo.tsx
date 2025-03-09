61a863145c9c36d4ca0c458f4019ce270068e82f
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-03-09 19:39:45 +0900

    Part 5. 할일 CRUD 기능 구현 & 1주차 미션 수행

---
+++ components/todo.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { Checkbox, IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteTodo, updateTodo } from "actions/todo-actions";
+import { queryClient } from "config/ReactQueryClientProvider";
+import { useState } from "react";
+
+export default function Todo({ todo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [completed, setCompleted] = useState(todo.completed);
+  const [title, setTitle] = useState(todo.title);
+  const [time, setTime] = useState(todo.created_at);
+  const [updatedTime, setUpdatedTime] = useState(todo.updated_at);
+
+  const updateTodoMutation = useMutation({
+    mutationFn: () =>
+      updateTodo({
+        id: todo.id,
+        title,
+        completed,
+        created_at: time,
+        updated_at: updatedTime,
+      }),
+    onSuccess: () => {
+      setIsEditing(false);
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
+
+  const deleteTodoMutation = useMutation({
+    mutationFn: () => deleteTodo(todo.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
+
+  return (
+    <div className="w-full flex items-center gap-1">
+      <Checkbox
+        checked={completed}
+        onChange={async (e) => {
+          await setCompleted(e.target.checked);
+          await updateTodoMutation.mutate();
+        }}
+      />
+
+      {isEditing ? (
+        <input
+          className="flex-1 border-b-black border-b pb-1"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      ) : (
+        <>
+          <p className={`flex-1 ${completed && "line-through"}`}>{title}</p>
+          <p>
+            {new Date(todo.updated_at ?? todo.created_at).toLocaleString(
+              "ko-KR",
+              {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "numeric",
+                minute: "2-digit",
+                hour12: true,
+              }
+            )}
+          </p>
+        </>
+      )}
+
+      {isEditing ? (
+        <IconButton
+          onClick={async () => {
+            await updateTodoMutation.mutate();
+          }}
+        >
+          {updateTodoMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-check" />
+          )}
+        </IconButton>
+      ) : (
+        <IconButton onClick={() => setIsEditing(true)}>
+          <i className="fas fa-pen" />
+        </IconButton>
+      )}
+
+      <IconButton onClick={() => deleteTodoMutation.mutate()}>
+        {deleteTodoMutation.isPending ? (
+          <Spinner />
+        ) : (
+          <i className="fas fa-trash" />
+        )}
+      </IconButton>
+    </div>
+  );
+}
